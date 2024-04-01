e05e0b329f2b20d15422af2b5939fb9b5d21e2cd
AlexanderStepanov05 <alexanderstepanov2002@mail.ru>
2024-04-01 21:56:56 +0300

    modify entities, add transaction type

--- src/main/java/org/stepanov/entity/Player.java
+++ src/main/java/org/stepanov/entity/Player.java
@@ -2,6 +2,21 @@ package org.stepanov.entity;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class Player {
+    private String username;
+    private String password;
+    private BigDecimal balance;
+    private List<Transaction> transactions;
+
+    public Player(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.balance = BigDecimal.valueOf(0.0);
+        this.transactions = new ArrayList<>();
+    }
 }
--- src/main/java/org/stepanov/entity/Transaction.java
+++ src/main/java/org/stepanov/entity/Transaction.java
@@ -1,7 +1,15 @@
 package org.stepanov.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.stepanov.types.TransactionType;
+
+import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
 public class Transaction {
+    private String id;
+    private BigDecimal amount;
+    private TransactionType type;
 }
